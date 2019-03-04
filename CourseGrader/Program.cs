c2a96800af034cc6019d755e8dc22e60d5e5b09b
h.c.ward1991@gmail.com <h.c.ward1991@gmail.com>
2019-03-04 17:56:42 -0600

    Testing Issues

--- CourseGrader/Program.cs
+++ CourseGrader/Program.cs
@@ -11,6 +11,40 @@ namespace CourseGrader
             // It should then return a string of "pass" or "fail" depending on these two conditions:
             // If the average score is greater than or equal to 70 and no single test score is below 50, then return a message of "pass".
             // If the average score is lower than 70 or at least one test score is below 50, then return a message of "fail".
+
+            int[] testScores = new int[] { 40, 92, 87, 77, 95, 95 };
+
+            Console.WriteLine(GradeTestScores(testScores));
+            Console.ReadLine();
+
+        }
+
+        public static string GradeTestScores(int[] testScores)
+        {
+            int total = 0;
+            int average = 0;
+            
+
+            foreach (int score in testScores)
+            { 
+                if (score < 50)
+                {
+                    return "Fail";
+                }
+
+                total += score;
+                average = total / testScores.Length;
+
+            }
+
+            if (average >= 70)
+            {
+                return "Pass";
+            }
+            else 
+            {
+                return "Fail";
+            }
         }
     }
 }
